91cfb333db978db91e6ec7849bd5fad9624364f0
Michael Chernigin <chernigin@altlinux.org>
2023-09-21 19:13:25 +0400

    refactor: db functions return result

--- src/db.rs
+++ src/db.rs
@@ -1,31 +1,41 @@
 use diesel::pg::PgConnection;
 use diesel::prelude::*;
-use dotenvy::dotenv;
-use std::env;
 
-use crate::models::{self, Paper};
+use crate::models;
+
+#[derive(thiserror::Error, Debug)]
+pub enum Error {
+    #[error("database connection error")]
+    ConnectionError(#[from] diesel::result::ConnectionError),
+
+    #[error("database query error")]
+    QueryError(#[from] diesel::result::Error),
+}
+
+pub type Result<T> = std::result::Result<T, Error>;
 
 pub struct DBConnection {
     pg: PgConnection,
 }
 
 impl DBConnection {
-    pub fn new() -> DBConnection {
-        dotenv().ok();
+    pub fn new() -> Result<DBConnection> {
+        dotenvy::dotenv().ok();
 
-        let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-        DBConnection {
-            pg: PgConnection::establish(&database_url)
-                .unwrap_or_else(|_| panic!("Error connecting to {}", database_url)),
-        }
+        let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+
+        Ok(DBConnection {
+            pg: PgConnection::establish(&database_url)?,
+        })
     }
 
-    pub fn get_all_papers(&mut self) -> Vec<models::Paper> {
+    pub fn get_all_papers(&mut self) -> Result<Vec<models::Paper>> {
         use crate::schema::papers::dsl::*;
 
-        papers
-            .select(Paper::as_select())
-            .load(&mut self.pg)
-            .expect("Error loading posts")
+        let result = papers
+            .select(models::Paper::as_select())
+            .load(&mut self.pg)?;
+
+        Ok(result)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -10,8 +10,8 @@ mod scraper;
 async fn main() -> anyhow::Result<()> {
     let cfg = config::Config::parse();
 
-    let mut db = db::DBConnection::new();
-    println!("{:#?}", db.get_all_papers());
+    let mut db = db::DBConnection::new()?;
+    println!("{:#?}", db.get_all_papers()?);
 
     let start_url: String = format!(
         "https://arxiv.org/search/advanced?advanced=&terms-0-operator=AND&terms-0-term=&terms-0-field=title&classification-computer_science=y&classification-physics_archives=all&classification-include_cross_list=include&date-filter_by=all_dates&date-year=&date-from_date=&date-to_date=&date-date_type=submitted_date&abstracts=show&size={}&order=announced_date_first",
